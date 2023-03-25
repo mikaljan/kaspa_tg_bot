cbb094712672973480859b505e21e111d681b856
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2023-03-25 20:29:42 +0100

    fixed tip int/float must be standalone

--- MAIN.py
+++ MAIN.py
@@ -778,7 +778,7 @@ def send_kas(e):
         return
 
     try:
-        amount = float(re.search(" (\d+([.,]\d+)?)[ $]?(KAS)?", e.text, re.IGNORECASE)[1].replace(",", "."))
+        amount = float(re.search(" (\d+([.,]\d+)?)[ $](KAS)?", e.text, re.IGNORECASE)[1].replace(",", "."))
     except Exception:
         msg = bot.send_message(e.chat.id, "Can't parse the amount.\n"
                                           "Reply to someone's message and write:\n `/tip X.XX KAS`.",
